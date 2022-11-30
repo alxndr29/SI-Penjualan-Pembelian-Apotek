1884aa3324717d56a901507345d4594d00899814
alxndr29 <alexevan2910@gmail.com>
2022-11-30 11:34:29 +0800

    Add Edit Draft Product Stop Opname

--- app/Http/Controllers/StockOpnameController.php
+++ app/Http/Controllers/StockOpnameController.php
@@ -153,26 +153,149 @@ class StockOpnameController extends Controller
 
     public function edit($id)
     {
-        $data = DB::table('detail_stock_opname as dso')
-            ->join('stock_opname as so', 'dso.stock_opname_id', '=', 'so.id')
-            ->join('products as p', 'dso.product_id', '=', 'p.id')
-            ->where('dso.id', '=', $id)
-            ->select(
-                'so.*',
-                'dso.*',
-                'p.*',
-                db::raw("(SELECT t.name  FROM product_types as t where t.id = p.product_type_id) as type"),
-                db::raw("(SELECT c.name  FROM product_categories as c where c.id = p.product_category_id) as category"),
-                db::raw("(SELECT u.name   FROM product_uom as u where u.id = p.product_uom_id) as uom"),
-                db::raw('(SELECT si.harga FROM stock_in AS si WHERE si.product_id = p.id AND si.jumlah > 0 ORDER BY CASE WHEN p.product_type_id = 1 then si.created_at END ASC, CASE WHEN p.product_type_id = 2 THEN si.expired_date END ASC LIMIT 1) AS harga'),
-                db::raw("(SELECT SUM(si.jumlah) FROM stock_in AS si WHERE si.product_id = dso.product_id) AS stok_barang")
-            )->get();
-        //        dd($data);
-        return view('pages.penyimpanan.stock-opname.edit', compact('data'));
+        // $data = DB::table('detail_stock_opname as dso')
+        //     ->join('stock_opname as so', 'dso.stock_opname_id', '=', 'so.id')
+        //     ->join('products as p', 'dso.product_id', '=', 'p.id')
+        //     ->where('dso.id', '=', $id)
+        //     ->select(
+        //         'so.*',
+        //         'dso.*',
+        //         'p.*',
+        //         db::raw("(SELECT t.name  FROM product_types as t where t.id = p.product_type_id) as type"),
+        //         db::raw("(SELECT c.name  FROM product_categories as c where c.id = p.product_category_id) as category"),
+        //         db::raw("(SELECT u.name   FROM product_uom as u where u.id = p.product_uom_id) as uom"),
+        //         db::raw('(SELECT si.harga FROM stock_in AS si WHERE si.product_id = p.id AND si.jumlah > 0 ORDER BY CASE WHEN p.product_type_id = 1 then si.created_at END ASC, CASE WHEN p.product_type_id = 2 THEN si.expired_date END ASC LIMIT 1) AS harga'),
+        //         db::raw("(SELECT SUM(si.jumlah) FROM stock_in AS si WHERE si.product_id = dso.product_id) AS stok_barang")
+        //     )->get();
+        $data = StockOpname::find($id)->first();
+        // $data2 = StockOpname::join('detail_stock_opname', 'stock_opname.id', '=', 'detail_stock_opname.stock_opname_id')
+        //     ->join('products', 'products.id', 'detail_stock_opname.product_id')
+        //     ->select('products.nama','products.id as idproducts', 'detail_stock_opname.*')
+        //     ->groupBy('products.id')
+        //     ->where('stock_opname.id', $id)
+        //     ->get();
+        $data2 = DB::table('get_product')
+            ->join('detail_stock_opname', 'get_product.id', '=', 'detail_stock_opname.product_id')
+            ->where('detail_stock_opname.stock_opname_id', $id)
+            ->select('get_product.*','get_product.id as idproduct', 'detail_stock_opname.*')
+            ->get();
+        // return $data2;
+        return view('pages.penyimpanan.stock-opname.edit', compact('data', 'data2'));
     }
 
     public function update(Request $request, $id)
-    { }
+    {
+        // return $id;
+        DB::beginTransaction();
+        // return $request->get('data_products');
+        // try {
+        if ($request->get('tombol') == 'draft') {
+            $stock_opname = StockOpname::where('id', '=', $id)->update([
+                'no_opname' => Carbon::parse($request->tanggal_mulai)->format('m'),
+                'bulan' => Carbon::parse($request->tanggal_mulai)->format('m'),
+                'tanggal_mulai' => $request->get('tanggal_awal_pemeriksaan'),
+                'tanggal_berakhir' => $request->get('tanggal_akhir_pemeriksaan'),
+                'operator' => \Auth::id(),
+                'state' => 'Draft'
+            ]);
+            foreach ($request->get('data_products') as $key => $value) {
+                $stock_komputer = StockIN::where('product_id', $key);
+                DB::table('detail_stock_opname')->where('stock_opname_id', '=', $id)->where('product_id', '=', $key)->update([
+                    // 'stock_opname_id' => $id,
+                    // 'product_id' => $key,
+                    'stock_computer' => $stock_komputer->sum('jumlah'),
+                    'stock_aktual' => $value,
+                    'stock_selisih' => $value - $stock_komputer->sum('jumlah'),
+                    'created_at' => date('Y-m-d'),
+                    'updated_at' => date('Y-m-d')
+                ]);
+            }
+        } else {
+            $stock_opname = StockOpname::where('id', '=', $id)->update([
+                'no_opname' => Carbon::parse($request->tanggal_mulai)->format('m'),
+                'bulan' => Carbon::parse($request->tanggal_mulai)->format('m'),
+                'tanggal_mulai' => $request->get('tanggal_awal_pemeriksaan'),
+                'tanggal_berakhir' => $request->get('tanggal_akhir_pemeriksaan'),
+                'operator' => \Auth::id(),
+                'state' => 'Finish'
+            ]);
+            foreach ($request->get('data_products') as $key => $value) {
+                $stock_komputer = StockIN::where('product_id', $key);
+                DB::table('detail_stock_opname')->where('stock_opname_id', '=', $id)->where('product_id', $key)->update([
+                    // 'stock_opname_id' => $id,
+                    // 'product_id' => $key,
+                    'stock_computer' => $stock_komputer->sum('jumlah'),
+                    'stock_aktual' => $value,
+                    'stock_selisih' => $value - $stock_komputer->sum('jumlah'),
+                    'created_at' => date('Y-m-d'),
+                    'updated_at' => date('Y-m-d')
+                ]);
+                $selisih = $value - $stock_komputer->sum('jumlah');
+                // return $selisih;
+                if ($value != $stock_komputer->sum('jumlah')) {
+                    $data = $stock_komputer->first();
+                    $result;
+                    if ($data->product_type_id == 1) {
+                        // array_push($result, StockIN::where('product_id', $key)->orderBy('expired_date', 'asc')->get());
+                        $result = StockIN::where('product_id', $key)->orderBy('expired_date', 'asc')->get();
+                    } else {
+                        // array_push($result, StockIN::where('product_id', $key)->orderBy('created_at', 'asc')->get());
+                        $result = StockIN::where('product_id', $key)->orderBy('created_at', 'asc')->get();
+                    }
+                    // return ($result);
+                    foreach ($result as $key => $value2) {
+                        if ($selisih < 0) {
+                            $stok = (int) $value2->jumlah;
+                            if ($stok != 0 && ($stok >= $selisih)) {
+                                $stok_in = StockIN::where('id', '=', $value2->id)->first();
+                                // $stok_in->jumlah = ($stok + $selisih);
+                                if (($stok + $selisih < 0)) {
+                                    $stok_in->jumlah = 0;
+                                    $stok_in->save();
+                                    $selisih = $stok + $selisih;
+                                } else {
+                                    // break;
+                                    $stok_in->jumlah = $stok + $selisih;
+                                    $stok_in->save();
+                                    $selisih = $stok + $selisih;
+                                    break;
+                                }
+                                // return $selisih;
+                                // return 'sini';
+                            } else if ($stok != 0 && ($stok < $selisih)) {
+                                $stok_in = StockIN::where('product_id', '=', $value2['id'])->where('id', '=', $value2->id)->first();
+                                $stok_in->jumlah = 0;
+                                $stok_in->save();
+                                $selisih = $selisih - $stok;
+                            } else { }
+                        } else if ($selisih > 0) {
+                            $stok = (int) $value2->jumlah;
+                            if ($stok != 0 && ($stok >= $selisih)) {
+                                $stok_in = StockIN::where('id', '=', $value2->id)->first();
+                                $stok_in->jumlah = ($stok + $selisih);
+                                $stok_in->save();
+                                $selisih = $stok + $selisih;
+                                break;
+                            } else if ($stok != 0 && ($stok < $selisih)) {
+                                $stok_in = StockIN::where('product_id', '=', $value2['id'])->where('id', '=', $value2->id)->first();
+                                $stok_in->jumlah = 0;
+                                $stok_in->save();
+                                $selisih = $selisih + $stok;
+                            } else { }
+                        } else { }
+                    }
+                }
+                // return $selisih;
+            }
+        }
+        DB::commit();
+        // return $request->get('data_products');
+        return redirect('penyimpanan/stock-opname/')->with(['success' => 'menambahkan edit baru']);
+        // } catch (\Exception $e) {
+        //     DB::rollback();
+        //     return $e->getMessage();
+        // }
+    }
 
     public function destroy($id)
     { }
--- resources/views/pages/penyimpanan/stock-opname/edit.blade.php
+++ resources/views/pages/penyimpanan/stock-opname/edit.blade.php
@@ -2,49 +2,46 @@
 @section('title', 'Stok Barang')
 
 @section('css')
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/datatables.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendors/datatables.css')}}">
 @endsection
 
 @section('breadcrumb-title')
-    <h3>Edit Stok Opname</h3>
+<h3>Mulai Stok Opname</h3>
 @endsection
 
 @section('breadcrumb-items')
-    <li class="breadcrumb-item">
-        Penyimpanan
-    </li>
-    <li class="breadcrumb-item"><a href="{{route('stock-opname.index')}}">Daftar Stock Opname</a></li>
-    <li class="breadcrumb-item active">Edit Stok Opname</li>
+<li class="breadcrumb-item">
+    Penyimpanan
+</li>
+<li class="breadcrumb-item"><a href="{{route('stock-opname.index')}}">Daftar Stock Opname</a></li>
+<li class="breadcrumb-item active">Mulai Stok Opname</li>
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
+<div class="container-fluid">
+    <div class="row">
+        <form action="{{route('stock-opname.update',$data->id)}}" method="post">
+            @csrf
+            @method('put')
             <div class="col-sm-12">
                 <div class="card p-4">
                     <div class="row">
                         <div class="col-3">
-                            <label class="form-label" for="exampleFormControlSelect9">Nomor Opname</label>
-                            <div class="input-group">
-                                <input type="input" disabled class="datepicker-here form-control digits" value="OPN-{{$data->first()->no_opname}}">
-                            </div>
-                        </div>
-                        <div class="col-3">
                             <label class="form-label" for="exampleFormControlSelect9">Tanggal Mulai Pemeriksaan</label>
                             <div class="input-group">
-                                <input class="datepicker-here form-control digits" type="date" value="{{$data->first()->tanggal_mulai}}">
+                                <input class="datepicker-here form-control digits" type="date" name="tanggal_awal_pemeriksaan" value="{{$data->tanggal_mulai}}">
                             </div>
                         </div>
                         <div class="col-3">
                             <label class="form-label" for="exampleFormControlSelect9">Tanggal Berakhir
                                 Pemeriksaan</label>
                             <div class="input-group">
-                                <input class="datepicker-here form-control digits" type="date" value="{{$data->first()->tanggal_berakhir}}">
+                                <input class="datepicker-here form-control digits" type="date" name="tanggal_akhir_pemeriksaan" value="{{$data->tanggal_berakhir}}">
                             </div>
                         </div>
-                        <div class="col-3 d-flex justify-content-end">
-                            <button class="btn btn-lg btn-outline-dark mt-4 me-2">Simpan sebagai draft</button>
-                            <button class="btn btn-lg btn-primary mt-4 ">Simpan Data</button>
+                        <div class="col-6 d-flex justify-content-end">
+                            <button class="btn btn-lg btn-outline-dark mt-4 me-2" type="submit" value="draft" name="tombol">Simpan sebagai draft</button>
+                            <button class="btn btn-lg btn-primary mt-4" type="submit" name="tombol">Simpan Data</button>
                         </div>
                     </div>
                 </div>
@@ -56,86 +53,45 @@
                         <div class="table-responsive">
                             <table class="cell-border" id="basic-1">
                                 <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Nama Barang</th>
-                                    <th>Jenis & Kategori</th>
-                                    <th>Harga Jual</th>
-                                    <th>Stok Sistem</th>
-                                    <th>Stok Aktual</th>
-                                </tr>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama Barang</th>
+                                        <th>Jenis & Kategori</th>
+                                        <th>Harga Jual</th>
+                                        <th>Stok Sistem</th>
+                                        <th>Stok Aktual</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                @php
-                                    $i = 0;
-                                @endphp
-                                @foreach($data as $product)
+                                    @foreach ($data2 as $key => $value)
                                     <tr>
-                                        <td>{{$i += 1}}</td>
-                                        <td>{{$product->nama}}</td>
-                                        <td><span class="fw-bold badge badge-info">{{$product->type}}</span> - {{$product->category}}</td>
-                                        <td>Rp. {{number_format($product->harga,0,',','.') }}</td>
-                                        <td><span id="stok_sistem">{{number_format($product->stok_barang,0,',','.') }} </span>{{$product->uom}}</td>
+
+                                        <td>{{$key+1}}</td>
+                                        <td>{{$value->nama}}</td>
+                                        <td><span class="fw-bold badge badge-info">{{$value->type}}</span> - {{$value->category}}</td>
+                                        <td>Rp. {{number_format($value->harga,0,',','.') }}</td>
+                                        <td><span id="stok_sistem">{{number_format($value->stok_barang,0,',','.') }} </span>{{$value->uom}}</td>
                                         <td>
                                             <div class="input-group">
-                                                <input class="form-control" type="number" min="0" value="{{$product->stok_barang}}" placeholder="Masukan Jumlah Stok Aktual"
-                                                       id="stok_aktual">
-                                                <span class="input-group-text" id="detail-produk-satuan">{{$product->uom}}</span>
+                                                <input value="{{$value->stock_aktual}}" class="form-control" type="number" min="0" value="{{$value->stok_barang}}" placeholder="Masukan Jumlah Stok Aktual" id="stok_aktual" name="data_products[{{$value->idproduct}}]">
+                                                <span class="input-group-text" id="detail-produk-satuan">{{$value->uom}}</span>
                                             </div>
                                         </td>
                                     </tr>
-                                @endforeach
-{{--                                @for($i = 1; $i<100;$i++)--}}
-{{--                                    <tr>--}}
-{{--                                        <td>1</td>--}}
-{{--                                        <td>Paramex {{$i}}</td>--}}
-{{--                                        <td><span class="fw-bold badge badge-info"></span> -</td>--}}
-{{--                                        <td>Rp. 15.000</td>--}}
-{{--                                        <td>5 Strip</td>--}}
-{{--                                        <td>--}}
-{{--                                            <div class="input-group">--}}
-{{--                                                <input class="form-control" type="number" min="0" value="0" placeholder="Masukan Jumlah Stok Aktual"--}}
-{{--                                                       id="jumlah-pembelian-produk">--}}
-{{--                                                <span class="input-group-text" id="detail-produk-satuan">UOM</span>--}}
-{{--                                            </div>--}}
-{{--                                        </td>--}}
-{{--                                    </tr>--}}
-{{--                                @endfor--}}
+                                    @endforeach
+                                <tbody>
 
-                                {{--                                @foreach($suppliers as $supplier)--}}
-                                {{--                                    <tr>--}}
-                                {{--                                        <td>{{$i+= 1}}</td>--}}
-                                {{--                                        <td>{{$supplier->name}}</td>--}}
-                                {{--                                        <td>{{$supplier->address}}</td>--}}
-                                {{--                                        <td>{{$supplier->telephone}}</td>--}}
-                                {{--                                        <td>--}}
-                                {{--                                            <span class="badge badge-{{$supplier->status == 0 ? 'danger' : 'success'}}">{{$supplier->status == 0 ? 'Tidak Aktif' : 'Aktif'}}</span>--}}
-                                {{--                                        </td>--}}
-                                {{--                                        <td>--}}
-                                {{--                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');"--}}
-                                {{--                                                  action="{{ route('supplier.destroy', $supplier->id) }}" method="POST">--}}
-                                {{--                                                <a href="{{route('supplier.edit', $supplier->id)}}" class="btn btn-warning btn-xl me-2">Edit</a>--}}
-                                {{--                                                @csrf--}}
-                                {{--                                                @method('DELETE')--}}
-                                {{--                                                <button class="btn btn-danger btn-xs" type="submit"--}}
-                                {{--                                                        data-original-title="btn btn-danger btn-xs" title=""--}}
-                                {{--                                                        data-bs-original-title="">Delete--}}
-                                {{--                                                </button>--}}
-                                {{--                                            </form>--}}
-
-                                {{--                                        </td>--}}
-                                {{--                                    </tr>--}}
-                                {{--                                @endforeach--}}
                             </table>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
+</div>
 @endsection
 
 @section('script')
-    <script src="{{asset('assets/js/datatable/datatables/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('assets/js/datatable/datatables/datatable.custom.js')}}"></script>
+<script src="{{asset('assets/js/datatable/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('assets/js/datatable/datatables/datatable.custom.js')}}"></script>
 @endsection
